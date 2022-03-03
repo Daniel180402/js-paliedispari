00757563a57c19d8b05bdefb68e271c429bcb45d
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-03 15:21:24 +0100

    added odd even game

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ let parolaInversa = invertiParola(word);
 
 if(word == parolaInversa){
     console.log('la parola è palindroma');
-    resultword.innerHTML = "la parola è palindroma"
+    resultword.innerHTML = "la parola è palindroma";
     } else {
     console.log('la parola non è palindroma');
-    resultword.innerHTML = "la parola non è palindroma"
+    resultword.innerHTML = "la parola non è palindroma";
     }
 
 function invertiParola(str){
@@ -15,3 +15,63 @@ function invertiParola(str){
     return strInversa;
 }
 
+const buttoneven = document.getElementById("btneven");
+let even;
+const buttonodd = document.getElementById("btnodd");
+let odd;
+
+
+buttoneven.addEventListener("click", function(){
+    console.log("even true")
+    return even = true
+})
+
+buttonodd.addEventListener("click", function(){
+    console.log("odd true")
+    odd = true
+})
+
+let usernum = document.getElementById("user-num");
+
+let pcnum = randomnum(1, 5);
+
+const buttonplay = document.getElementById("btnplay");
+
+buttonplay.addEventListener("click", function(){
+    console.log(odd);
+    console.log(even);
+    console.log(usernum.value);
+    console.log(pcnum);
+    let winner = document.getElementById("winner");
+    let sumnum = usernum.value + pcnum;
+    console.log(sumnum);
+    if (sumnum % 2 === 0){
+        console.log("il risultato è pari");
+        if(even == true){
+            console.log("hai vinto!");
+            winner.innerHTML = "hai vinto!";
+        }
+        else if(odd == true){
+            console.log("hai perso!");
+            winner.innerHTML = "hai perso!";
+        }
+    }
+    else{
+        console.log("il risultato è dispari");
+        if(odd == true){
+            console.log("hai vinto!")
+            winner.innerHTML = "hai vinto!";
+        }
+        else if(even == true){
+            console.log("hai perso!");
+            winner.innerHTML = "hai perso!";
+        }
+    }
+    
+})
+
+function randomnum(min, max){
+
+    let pcnum = Math.floor(Math.random() * max) + min;
+    return pcnum;
+}
